de7d82fb44880d782f8f04e2712d8c4b847a54ff
F4llenDeath <206588810+F4llenDeath@users.noreply.github.com>
2026-03-12 17:00:07 -0500

    Initial Commit

---
+++ Smart Walker/Smart_WalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_WalkerApp.swift
+//  Smart Walker
+//
+//  Created by Xicheng Yang on 2026/3/12.
+//
+
+import SwiftUI
+
+@main
+struct Smart_WalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smart WalkerTests/Smart_WalkerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_WalkerTests.swift
+//  Smart WalkerTests
+//
+//  Created by Xicheng Yang on 2026/3/12.
+//
+
+import Testing
+@testable import Smart_Walker
+
+struct Smart_WalkerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
